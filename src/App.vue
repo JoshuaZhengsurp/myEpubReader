1e2c905c2d1a8f947c67e4f29dfc2fc3d1266281
JoshuaZheng <3107641910@qq.com>
2023-06-27 14:52:37 +0800

    "6/27/23/1"

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <HelloWorld></HelloWorld>
+    <span class="icon-back"></span>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+document.addEventListener('DOMContentLoaded',()=>{
+    const html:HTMLElement = document.querySelector('html');
+    let fontSize = window.innerWidth/10;
+    fontSize = fontSize > 40?40:fontSize;
+    console.log(fontSize)
+    html.style.fontSize = fontSize + 'px';
+})
+</script>
+
+<style>
 </style>
